b551fb10333721cae3b2ef185792e9619732d2a3
HoangViet <hoangphamhl000@gmail.com>
2023-11-19 09:54:03 +0700

    update markdown

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -4,6 +4,23 @@ import { connect } from 'react-redux';
 import './TableManageUser.scss';
 import * as actions from '../../../store/actions';
 
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import 'react-markdown-editor-lite/lib/index.css';
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+    console.log('handleEditorChange', html, text);
+}
+
+
 class TableManageUser extends Component {
 
     constructor(props) {
@@ -36,36 +53,40 @@ class TableManageUser extends Component {
     render() {
         let arrUser = this.state.tblManageUser;
         return (
-            <table id='TableManageUser'>
-                <tbody>
-                    <tr>
-                        <th>Email</th>
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>Address</th>
-                        <th>Actions</th>
-                    </tr>
-                    {arrUser && arrUser.length > 0 &&
-                        arrUser.map((item, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>{item.email}</td>
-                                    <td>{item.firstName}</td>
-                                    <td>{item.lastName}</td>
-                                    <td>{item.address}</td>
-                                    <td>
-                                        <button className='btn-edit' onClick={() => this.handleEditUser(item)}>
-                                            <i className="fas fa-pencil-alt"></i></button>
+            <React.Fragment>
+                <table id='TableManageUser'>
+                    <tbody>
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        {arrUser && arrUser.length > 0 &&
+                            arrUser.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit' onClick={() => this.handleEditUser(item)}>
+                                                <i className="fas fa-pencil-alt"></i></button>
 
-                                        <button className='btn-delete' onClick={() => this.handleDeleteUser(item)}>
-                                            <i className="fas fa-trash-alt"></i></button>
-                                    </td>
-                                </tr>
-                            )
-                        })}
+                                            <button className='btn-delete' onClick={() => this.handleDeleteUser(item)}>
+                                                <i className="fas fa-trash-alt"></i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+
+                <MdEditor style={{ height: '500px' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
+            </React.Fragment>
         );
     }
 
